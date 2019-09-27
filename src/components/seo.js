811b1c740b724595850a61fb12c50ddc0a6d6c06
peterbaile <peterchenwflms@gmail.com>
2019-09-26 22:51:54 -0400

    update metadata

--- src/components/seo.js
+++ src/components/seo.js
@@ -3,7 +3,14 @@ import { Helmet } from 'react-helmet'
 
 const SEO = ({ title }) => (
   <Helmet>
-    <meta charSet="utf-8" name="Peter Baile Chen" content="Personal Website for Peter Baile Chen. Currently, a NETS students studying at University of Pennsylvania(UPenn)" />
+    <meta charSet="utf-8" />
+    <meta property="og:type" content="website" />
+    <meta property="og:site_name" content="Peter Baile Chen" />
+    <meta property="og:url" content="https://peterchen.herokuapp.com/" />
+    <meta property="twitter:url" contet="https://peterchen.herokuapp.com/" />
+    <meta name="author" content="Peter Baile Chen" />
+    <meta name="description" content="Personal Website for Peter Baile Chen. Currently, a NETS students studying at University of Pennsylvania (UPenn)" />
+    <meta name="keywords" content="peter, peter baile, peter baile chen, chen, baile, upenn, university of pennslyvania, penn, nets, penn labs, labs, 197, cis 197, cis-197, penn basics, basics, web, web developer, developer" />
     <title>
       {!title ? 'Peter Baile Chen' : `Peter Baile Chen | ${title}`}
     </title>
