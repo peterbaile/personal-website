23675694fb7abad1bdf3af1f60664879539da2d0
peterbaile <peterchenwflms@gmail.com>
2021-08-28 12:41:49 -0400

    add research page

--- src/pages/research.js
+++ src/pages/research.js
@@ -3,27 +3,38 @@ import React from 'react'
 import Nav from '../components/Nav'
 import SEO from '../components/seo'
 
-import ProfJSON from '../json/professional.json'
+import ResearchJSON from '../json/research.json'
 
-const clubs = ProfJSON.slice(0, 2)
-const teaching = ProfJSON.slice(2)
+const { paper, projects } = ResearchJSON
 
-const Experience = ({ head, img, desc, time }) => (
-  <div className="row" style={{ marginBottom: '1.5rem' }}>
-    <div className="col-md-1">
-      <img src={`/images/${img}`} className="img-fluid" width="50px" />
-    </div>
-    <div className="col-md-11">
-      <b style={{ fontSize: '1.2rem' }}> {head} &#124;</b>&nbsp;
-      <text style={{ color: '#677885', fontSize: '90%' }}>{time}</text>
-      {Boolean(desc.length) && (
-        <ul>
-          {desc.map(e => (
-            <li> {e} </li>
-          ))}
-        </ul>
-      )}
-    </div>
+const Paper = ({ title, authors, conference, links }) => (
+  <>
+    <p style={{ fontSize: '1.2rem' }}>
+      <b> {title} </b>
+    </p>
+    <p style={{ fontSize: '0.9rem' }}>
+      <em> {conference} </em>
+    </p>
+    <p style={{ fontSize: '0.9rem' }}> {authors} </p>
+    {Object.keys(links).map(k => (
+      <a href={`${links[k]}`}> [{k}] </a>
+    ))}
+  </>
+)
+
+const Project = ({ head, desc, links }) => (
+  <div style={{ marginBottom: '1.5rem' }}>
+    <b style={{ fontSize: '1.2rem' }}> {head} </b>
+    {Boolean(desc.length) && (
+      <ul>
+        {desc.map(e => (
+          <li> {e} </li>
+        ))}
+      </ul>
+    )}
+    {Object.keys(links).map(k => (
+      <a href={`${links[k]}`}> [{k}] </a>
+    ))}
   </div>
 )
 
@@ -38,13 +49,14 @@ const Research = () => (
           className="col-md-9"
           style={{ marginTop: '5em', padding: '0 5rem' }}
         >
-          Clubs
-          {clubs.map(e => (
-            <Experience {...e} />
+          Paper
+          {paper.map(e => (
+            <Paper {...e} />
           ))}
-          Teaching
-          {teaching.map(e => (
-            <Experience {...e} />
+          <div style={{ margin: '3rem 0' }} />
+          <h2> Projects </h2>
+          {projects.map(e => (
+            <Project {...e} />
           ))}
         </div>
       </div>
